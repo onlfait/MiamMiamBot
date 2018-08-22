daf2ce99a24d030bade0890fa8c158b21856dbeb
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2018-08-22 07:50:35 +0200

    check for no port found

--- App/renderer.js
+++ App/renderer.js
@@ -1,17 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-//require('bulma/css/bulma.css')
 
 const serialport = require('serialport')
 const pkg = require('./package.json')
 
 const $ports = document.getElementById('ports')
 const $refreshPorts = document.getElementById('refreshPorts')
+const $connect = document.getElementById('connect')
 
 document.title = `${pkg.name} - v${pkg.version}`
 
 $refreshPorts.addEventListener('click', updatePorts, false)
+$connect.addEventListener('click', connect, false)
 
 function updatePorts () {
   serialport.list((err, ports) => {
@@ -25,20 +26,40 @@ function updatePorts () {
     }
 
     if (ports.length === 0) {
-      document.getElementById('error').textContent = 'Pas de ports'
+      $ports.textContent = ''
+      addPort('null', 'Aucun port trouvé...')
+    } else {
+      setPorts(ports)
     }
-
-    $ports.textContent = ''
-
-    ports.forEach(port => {
-      let $option = document.createElement('option')
-      $option.textContent = `${port.comName} - ${port.manufacturer}`
-      $option.value = port.comName
-      $option.title = port.comName
-      $ports.appendChild($option)
-      //document.getElementById('ports').innerHTML += port.comName + '<br />'
-    })
   })
+}
+
+function addPort (value, label) {
+  let $option = document.createElement('option')
+  $option.textContent = label || value
+  $option.value = value
+  $ports.appendChild($option)
+}
+
+function addPorts (ports) {
+  ports.forEach(port => {
+    addPort(port.comName, `${port.comName} - ${port.manufacturer}`)
+  })
+}
+
+function setPorts (ports) {
+  $ports.textContent = ''
+  addPorts(ports)
+}
+
+function connect () {
+  let port = $ports.value
+
+  if (port === 'null') {
+    return
+  }
+
+  console.log($ports.value);
 }
 
 updatePorts();
